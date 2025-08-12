dabc433eb5a701727797936aa2c8f68310a554b5
OlehKniaziev <atmatm9182@gmail.com>
2025-08-12 12:35:41 +0200

    Added an option to run a file as a script

--- src/main.c
+++ src/main.c
@@ -4,9 +4,58 @@
 #include "print.h"
 #include "common.h"
 
-int main() {
+static void AnanasEvalFile(AnanasArena *arena, HeliosStringView file_path) {
+    HeliosAllocator allocator = AnanasArenaToHeliosAllocator(arena);
+
+    HeliosStringView file_contents = HeliosReadEntireFile(allocator, file_path);
+    if (file_contents.data == NULL) {
+        fprintf(stderr, "Failed to read the file\n");
+        exit(1);
+    }
+
+    HeliosString8Stream source;
+    HeliosString8StreamInit(&source, file_contents.data, file_contents.count);
+
+    AnanasLexer lexer;
+    AnanasLexerInit(&lexer, &source);
+
+    U8 backing_error_buffer[1024];
+    HeliosStringView error_buffer = {.data = backing_error_buffer, .count = sizeof(backing_error_buffer)};
+    AnanasErrorContext error_ctx = {.ok = 1, .place = HELIOS_SV_LIT("repl"), .error_buffer = error_buffer};
+
+    AnanasReaderTable reader_table;
+    AnanasReaderTableInit(&reader_table, allocator);
+
+    AnanasEnv env;
+    AnanasEnvInit(&env, NULL, allocator);
+    AnanasRootEnvPopulate(&env);
+
+    AnanasValue node;
+    while (AnanasReaderNext(&lexer, &reader_table, arena, &node, &error_ctx)) {
+        AnanasValue result;
+        if (!AnanasEval(node, arena, &env, &result, &error_ctx)) {
+            fprintf(stderr, "Eval error: " HELIOS_SV_FMT "\n", HELIOS_SV_ARG(error_ctx.error_buffer));
+            exit(1);
+        }
+    }
+
+    if (!error_ctx.ok) {
+        fprintf(stderr, "Reader error: " HELIOS_SV_FMT "\n", HELIOS_SV_ARG(error_ctx.error_buffer));
+        exit(1);
+    } else {
+        exit(0);
+    }
+}
+
+int main(int argc, char **argv) {
     AnanasArena arena;
     AnanasArenaInit(&arena, 64 * 1024 * 1024);
+
+    if (argc >= 2) {
+        HeliosStringView file_path = HELIOS_SV_LIT(argv[1]);
+        AnanasEvalFile(&arena, file_path);
+        HELIOS_UNREACHABLE();
+    }
 
     HeliosAllocator arena_allocator = AnanasArenaToHeliosAllocator(&arena);
 
