ee168eea5dee91884ee21a1e5b250fdcaf6c0ce9
OlehKniaziev <atmatm9182@gmail.com>
2025-08-13 23:41:24 +0200

    Added `AnanasPrintStdout` function

--- src/print.h
+++ src/print.h
@@ -6,4 +6,10 @@
 
 HeliosStringView AnanasPrint(HeliosAllocator allocator, AnanasValue value);
 
+static inline void AnanasPrintStdout(AnanasValue value) {
+    HeliosAllocator temp = HeliosGetTempAllocator();
+    HeliosStringView string = AnanasPrint(temp, value);
+    printf(HELIOS_SV_FMT "\n", HELIOS_SV_ARG(string));
+}
+
 #endif // ANANAS_PRINT_H_
