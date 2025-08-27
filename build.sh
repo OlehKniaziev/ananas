729d011d3b419d4013e0a9afa8acdf9ef2be4ffa
OlehKniaziev <atmatm9182@gmail.com>
2025-08-27 19:49:25 +0200

    Added different build types for the interpreter

--- build.sh
+++ build.sh
@@ -2,4 +2,13 @@
 
 set -xe
 
-clang -o ananas -Wall -Wextra -Werror -O0 -g ./src/main.c ./src/lexer.c ./src/read.c ./src/astron.c ./src/common.c ./src/eval.c ./src/print.c
+commonflags="-Wall -Wextra -Werror -g"
+sources="./src/main.c ./src/lexer.c ./src/read.c ./src/astron.c ./src/common.c ./src/eval.c ./src/print.c"
+
+if [ "$1" = "release" ]; then
+    clang -o ananas $commonflags -O2 $sources
+elif [ "$1" = "san" ]; then
+    clang -o ananas $commonflags -fsanitize=address -DANANAS_REPLACE_ARENA_WITH_MALLOC -O0 $sources
+else
+    clang -o ananas $commonflags -O0 $sources
+fi
